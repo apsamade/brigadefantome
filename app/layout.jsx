5d28c90e352456d84ed2648a983b18a3bed99916
apsamade <bouderga.abdessamade@gmail.com>
2024-04-19 01:01:14 +0200

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,4 @@
 import '@styles/global.css'
-import { LayoutRouter } from 'next/dist/server/app-render/entry-base'
 
 export const metadata = {
     title: 'Brigade Fantôme',
@@ -7,14 +6,24 @@ export const metadata = {
 }
 
 const RootLayout = ({ children }) => {
-    return(
+    return (
         <html lang="fr">
-        <head>
-            <meta charset="UTF-8" />
-        </head>
-        <body>
-            {children}
-        </body>
+            <head>
+                <meta name="author" content="Abdel-Samade Bouderga" />
+                <meta name="robots" content="index, follow" />
+                <meta property="og:type" content="website" />
+                <meta property="og:locale" content="fr_FR" />
+                <link rel="shortcut icon" href="/logo/logo_brigade_2.png" type="image/x-icon" />
+                <meta charset="UTF-8" />
+                <meta property="og:title" content="Tournoi de la Brigade Fantôme" />
+                <meta property="og:description" content="Site officiel de la Brigade Fantôme organisation de tournoi de jeux vidéo compétitif tel que Warzone / League of Legends / NBA et autre." />
+                <meta property="og:image" content="/logo/logo_brigade_2.png" />
+                <meta property="og:url" content="https://brigadefantome.vercel.app/" />
+
+            </head>
+            <body>
+                {children}
+            </body>
         </html>
     )
 }
