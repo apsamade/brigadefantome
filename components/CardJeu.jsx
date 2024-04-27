ef83dbd1a6b40a3290220a83b56aece34e3b40ae
apsamade <bouderga.abdessamade@gmail.com>
2024-04-27 21:11:47 +0200

    maj

--- components/CardJeu.jsx
+++ components/CardJeu.jsx
@@ -11,9 +11,9 @@ const CardJeu = ({image, nom, description, id}) => {
                 height={800}
                 className="rounded-md object-contain xl:object-cover xl:h-[100%]"
             />
-            <div className="absolute opacity-0 flex flex-col justify-end hover:opacity-100 duration-200 p-4 overflow-hidden  btn-decouvrir left-0 bottom-0 right-0 top-0 rounded-lg">
-                <h3 className="text-3xl">{nom}</h3>
-                <p className="mt-1 mb-4 font-light">{description}</p>
+            <div className="absolute lg:opacity-0 flex flex-col justify-end hover:opacity-100 duration-200 p-4 overflow-hidden  btn-decouvrir left-0 bottom-0 right-0 top-0 rounded-lg">
+                <h3 className="text-3xl whitespace-nowrap overflow-hidden text-ellipsis">{nom}</h3>
+                <p className="mt-1 mb-4 font-light text-ellipsis">{description}</p>
                 <Link
                     href={'/dashboard/decouvrir/jeu/' + id}
                     className="py-3 uppercase font-light hover:px-16 duration-200 hover:bg-black hover:text-white w-fit px-8 bg-white text-black rounded-md block text-center my-1"
