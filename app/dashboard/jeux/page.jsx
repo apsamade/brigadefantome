245bd180e693b1dbd52cb0a033febcc577d43ebf
apsamade <bouderga.abdessamade@gmail.com>
2024-04-28 22:04:08 +0200

    maj

--- app/dashboard/jeux/page.jsx
+++ app/dashboard/jeux/page.jsx
@@ -28,7 +28,7 @@ const Jeux = () => {
             <section className="flex w-[100%] flex-wrap xl:flex-nowrap max-w-[1200px] min-h-[400px] xl:h-[400px] mx-auto items-center justify-center p-4">
                 {jeux.map((jeu) => ( 
                     <CardJeu
-                        classChange={"shadow-2xl xl:h-[100%] overflow-hidden basis-[300px] grow xl:flex-[1.5] xl:hover:flex-[6] duration-200 m-1 rounded-md relative"}
+                        classChange={"shadow-2xl xl:h-[100%] overflow-hidden basis-[400px] grow duration-200 m-1 rounded-md relative"}
                         key={jeu._id}
                         nom={jeu.nom}
                         description={jeu.description}
