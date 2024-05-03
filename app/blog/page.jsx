6e4140ffed9d771663c151ad396a41e99cad17f2
apsamade <bouderga.abdessamade@gmail.com>
2024-05-03 23:13:37 +0200

    maj

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Blog = () => {
     return (
-        <div>Blog</div>
+        <main className='grow min-h-[70vh]'>
+            <h1 className='py-12 text-center mx-auto text-3xl uppercase'>Blog en construction ...</h1>
+        </main>
     )
 }
 
